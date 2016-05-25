0990bf2fbd7d3e2d7ac00bd6c55c88b43d243af0
Thom Allen <thomwork@gmail.com>
2016-05-25 15:14:43 +0100

    Initial + basic Search component

---
+++ app/scripts/App.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+import Search from './components/Search';
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <Search />
+//     );
+//   }
+// }
+
+// stateless functional component version of above
+const App = () => <Search />;
+export default App;
---
+++ app/scripts/components/Search.js
@@ -0,0 +1,96 @@
+/* eslint-disable no-console, no-alert */
+
+import React from 'react';
+import 'whatwg-fetch';
+import fetchJsonp from 'fetch-jsonp';
+
+export default class Search extends React.Component {
+  constructor() {
+    super();
+    // set initial state
+    this.state = {
+      results: [],
+      query: null,
+    };
+    this.storedKey = null;
+    // manual bindings
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.getKey = this.getKey.bind(this);
+  }
+  componentDidMount() {
+    this.getKey();
+  }
+  // get api key from local file
+  getKey() {
+    fetch('keys.json', {
+      method: 'get',
+      dataType: 'json',
+    }).then((response) => {
+      return response.json();
+    }).then((json) => {
+      // store the key for use in api queries
+      this.storedKey = json.keys.moviesdb;
+    }).catch((err) => {
+      alert(err);
+    });
+  }
+  // update the state from input value
+  handleKeyUp(e) {
+    console.log(e.target.value);
+    this.setState({
+      query: e.target.value,
+    }, this.apiQuery(this.storedKey, e.target.value));
+    // if (e.key === 'Enter') {
+    // }
+  }
+  // send the api get request
+  apiQuery(key, query) {
+    console.log(`apiQuery called with ${query}`);
+    fetchJsonp(`https://api.themoviedb.org/3/search/multi?api_key=${key}&query=${query}`, {
+      method: 'get',
+      http: {
+        headers: {
+          'Api-User-Agent':
+          'Medias 0.1 (https://github.com/tjallen/medias); thomwork@gmail.com',
+        },
+      },
+    }).then((response) => response.json()
+    ).then((json) => {
+      if (json.results.length > 0) {
+        console.log(json.results.length, ' results');
+        this.setState({
+          results: json.results,
+        });
+      } else {
+        console.log('no results');
+      }
+    }).catch((err) => {
+      alert(err);
+    });
+  }
+  // probably standardise results.title etc as in vue here
+  // filterApiResults(results) {
+  //   for (const result of results) {
+  //     console.log(result);
+  //   }
+  // }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search for stuff"
+          value={this.state.value}
+          onSubmit={this.handleKeyUp}
+          onKeyUp={this.handleKeyUp}
+        >
+        </input>
+        <ul>
+          {this.state.results.map((result, index) => {
+            return <li key={index}>{result.id} {result.title} {result.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}

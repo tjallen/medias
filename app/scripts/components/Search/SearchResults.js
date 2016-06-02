7be172e517884361989c8c30640c3e05e7a36b54
Thom Allen <thomwork@gmail.com>
2016-06-02 01:41:59 +0100

    Convert SearchResults comp to stateless function

--- app/scripts/components/Search/SearchResults.js
+++ app/scripts/components/Search/SearchResults.js
@@ -1,31 +1,26 @@
-/* eslint-disable prefer-stateless-function */
-// ^ this component as a stateless function breaks props, NAUGHTY ESLINT?!?!
+import React, { PropTypes } from 'react';
 
-import React, { Component, PropTypes } from 'react';
+const SearchResults = (props) =>
+  <div>
+    {props.results.length > 1 ? <p>{props.results.length} results</p> : null}
+    <ul>
+      {props.results.map((result, index) =>
+        <li
+          key={result.id}
+          onClick={() => props.onClick(result.id, index)}
+        >
+          Name: {result.name}<br />
+          Language: {result.original_language}<br />
+          Type: {result.media_type}<br />
+          Year: {result.date}<br />
+        </li>
+      )}
+    </ul>
+  </div>;
 
-export default class SearchResults extends Component {
-  static propTypes = {
-    results: PropTypes.array.isRequired,
-    onClick: PropTypes.func.isRequired,
-  }
-  render() {
-    return (
-      <div>
-        {this.props.results.length > 1 ? <p>{this.props.results.length} results</p> : null}
-        <ul>
-          {this.props.results.map((result, index) =>
-            <li
-              key={result.id}
-              onClick={() => this.props.onClick(result.id, index)}
-            >
-              Name: {result.name}<br />
-              Language: {result.original_language}<br />
-              Type: {result.media_type}<br />
-              Year: {result.date}<br />
-            </li>
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
+SearchResults.propTypes = {
+  results: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default SearchResults;
